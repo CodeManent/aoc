d051815a60c558e366efa26a2665c89de4fc1d56
Αλέξανδρος Παπαγεωργίου <oriong85@gmail.com>
2019-12-08 21:05:12 +0200

    Day 8 fixes
    Formatting
    Color enum out of main

--- 2019/day/8/day8.cpp
+++ 2019/day/8/day8.cpp
@@ -17,12 +17,19 @@ struct Dimmentions{
 typedef std::vector<int> Layer; // should be vector_view
 typedef std::vector<int> Image;
 
+enum Color {
+	Black = 0,
+	White,
+	Transparent
+};
+
 int main(int, char**){
 	std::cout << "Day 8" << std::endl;
 
 	std::vector<int> input;
 	std::transform(
-		std::istream_iterator<char>(std::cin), std::istream_iterator<char>(),
+		std::istream_iterator<char>(std::cin),
+		std::istream_iterator<char>(),
 		std::back_inserter(input),
 		[](const char ch){
 			return int(ch) - '0';
@@ -35,8 +42,8 @@ int main(int, char**){
 
 	for(auto i = decltype(layerCount){0}; i < layerCount; ++i) {
 		layers.emplace_back(
-			input.begin() + (i * d.size()),
-			input.begin() + ((i+1) * d.size()));
+			input.cbegin() + (i * d.size()),
+			input.cbegin() + ((i+1) * d.size()));
 	}
 
 	std::map<std::pair<const Layer * const, const int>, const int> countMemory;
@@ -54,32 +61,31 @@ int main(int, char**){
 		return result;
 	};
 
-	const auto &selected = std::min_element(layers.cbegin(), layers.cend(), [&count](const auto &l1, const auto &l2) {
-		return count(l1, 0) < count(l2, 0);
-	});
+	const auto &selected = std::min_element(
+		layers.cbegin(),
+		layers.cend(),
+		[&count](const auto &l1, const auto &l2) {
+			return count(l1, 0) < count(l2, 0);
+		});
 
 	const auto result = count(*selected, 1) * count(*selected, 2);
 
 	std::cout << "Result: " << result << std::endl;
 
 	// Render Image
-	enum Color {
-		Black = 0,
-		White,
-		Transparent
-	};
 
 	const auto image = std::accumulate(
-		layers.crbegin(), layers.crend(),
-		Image(layers.front().size(), Color::Black),
+		layers.crbegin(),
+		layers.crend(),
+		Image(d.size(), Color::Black),
 		[](auto &image, const auto &layer) {
 			// apply layer onto image
 			std::transform(
 				image.begin(), image.end(),
 				layer.begin(),
 				image.begin(),
-				[](const auto &imageValue, const auto &layerValue){
-					return layerValue == Color::Transparent ? imageValue : layerValue;
+				[](const auto &imgValue, const auto &lValue){
+					return lValue == Color::Transparent ? imgValue : lValue;
 				});
 			return image;
 		});
